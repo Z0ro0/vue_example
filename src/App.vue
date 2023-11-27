1801d54b1c8ad9588eab60be8652f29471c1df7f
Z0ro0 <chlrkdmf0805@gmail.com>
2023-11-27 10:00:43 +0900

    예제 백업

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
+  <div class="menu">
+    <a v-for="menubar in menuitems" :key="menubar">{{menubar}}</a>
+  </div>
+
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>80만원</p>
+    <button @click="bbippoclick">허위매물신고</button>
+    <span>신고수 :{{ bbippo  }}</span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>50만원</p>
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menuitems: ['home', 'about', 'products'],
+      bbippo: 0,
+    }
+  },
+  methods: {
+    bbippoclick(){
+      this.bbippo++;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +49,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu{
+  background: darkblue ;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>

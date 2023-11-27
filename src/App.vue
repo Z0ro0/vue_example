15f9dd88d48c22a1c633f0309c0538934d16b4e0
Z0ro0 <chlrkdmf0805@gmail.com>
2023-11-27 10:25:46 +0900

    모달창 예제

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,36 @@
     <a v-for="menubar in menuitems" :key="menubar">{{menubar}}</a>
   </div>
 
+
+  <div class="black-bg" v-if="modal==true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="modal=false">닫기</button>
+    </div>
+  </div>
+
+
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room1.jpg">
+    <h4 @click="modal=true">{{products[0]}}</h4>
     <p>80만원</p>
     <button @click="bbippoclick">허위매물신고</button>
-    <span>신고수 :{{ bbippo  }}</span>
+    <span>신고수 :{{ bbippo[0]  }}</span>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
+    <img src="./assets/room2.jpg">
+    <h4 @click="modal=true">{{products[1]}}</h4>
     <p>70만원</p>
+    <button @click="bbippoclick">허위매물신고</button>
+    <span>신고수 :{{ bbippo[1]  }}</span>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
+    <img src="./assets/room3.jpg">
+    <h4 @click="modal=true">{{products[2]}}</h4>
     <p>50만원</p>
+    <button @click="bbippoclick">허위매물신고</button>
+    <span>신고수 :{{ bbippo[2]  }}</span>
   </div>
 </template>
 <script>
@@ -29,7 +46,8 @@ export default {
     return{
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       menuitems: ['home', 'about', 'products'],
-      bbippo: 0,
+      bbippo: [0,0,0],
+      modal: false,
     }
   },
   methods: {
@@ -43,12 +61,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20pxs;
 }
 .menu{
   background: darkblue ;
